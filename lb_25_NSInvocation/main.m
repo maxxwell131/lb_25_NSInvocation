450c5aca8b1c46190a08e2fbf9b36691bfe22ff0
maxxwell131 <maxxwell131@yahoo.com>
2017-05-02 15:07:24 +0300

    Initial Commit

---
+++ lb_25_NSInvocation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lb_25_NSInvocation
+//
+//  Created by admin on 5/2/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
